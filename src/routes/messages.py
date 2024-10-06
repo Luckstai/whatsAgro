17aedae57000f770bdd8d36ba38a4539e518aac6
lucas henrique <lucas.henrique.tai@gmail.com>
2024-10-05 23:21:59 -0300

    fix log

--- src/routes/messages.py
+++ src/routes/messages.py
@@ -4,13 +4,14 @@ router = APIRouter(prefix="/messages", tags=["Messages"])
 
 @router.post("/")
 async def received_messages(request: Request):
+    print('CHEGOU MENSAGEM')
     # Log ou processar a mensagem
     # Acessar os parâmetros enviados no corpo da requisição
     form_data = await request.form()
     
     # Logar cada parâmetro recebido
     for key, value in form_data.items():
-        logging.info(f"{key}: {value}")
+        print(f"{key}: {value}")
     
     # # Verificar se há mídia anexada
     # if int(NumMedia) > 0 and MediaUrl0 and MediaContentType0:
